edb690361be974177b54bcb2b12b36989d5c7bcc
Adrien Guo <gps.adrien@gmail.com>
2022-01-17 17:32:57 +0800

    edit DAE

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
+
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.optimizers import SGD
 from keras.utils import plot_model
 
 directory = "D:\Data\g-research-crypto-forecasting\\"
@@ -9,24 +10,64 @@ directory = "D:\Data\g-research-crypto-forecasting\\"
 train_path = directory + "train.csv"
 asset_path = directory + "asset_details.csv"
 
+
+
 train_data = pd.read_csv(train_path)
-print(train_data[-750400:])
-print(train_data[-750400:].isna().sum())
+print(train_data[:60])
+print(train_data[:60].isna().sum())
+print(type(train_data))
 
 # +---------------------------------------+
 # |   DAE                                 |
 # +---------------------------------------+
-class DAE:
-    def model(dim, ):
+class DenoisingAE:
+    def __init__(self):
+        pass
+
+    def gaussian_noise(self, shape):
+        mu, sigma = 0, 0.01
+        noise = np.random.normal(mu, sigma, shape)
+        return noise
+
+    def create_model(self, in_dim, out_dim):
+        model = Sequential()
+
         # encoder
-        input_vec = Input(shape=(dim,))
+        model.add(Dense(units=5, input_dim=in_dim, activation="relu"))
 
         # decoder
+        model.add(Dense(units=out_dim))
+
+        plot_model(model, to_file='autoencoder.png', show_shapes=True)
+        return model
+
+    def train(self, data, label):
+        in_dim = data.shape[1]
+        out_dim = label.shape[1]
+        self.model = self.create_model(in_dim, out_dim)
+        self.model.compile(optimizer="adam", loss="mse")
+        data = data + self.gaussian_noise(data.shape)               # add gaussian noise to data
+        train_history = self.model.fit(x=data, y=label, epochs=10, batch_size=2)
+    
+    def predict(self, test):
+        predictions = self.model.predict(test)
+        print(predictions)
 
 
-encoder = Sequential([Dense(2, input_shape=[3])])
-decoder = Sequential([Dense(3, input_shape=[2])])
-autoencoder = Sequential([encoder, decoder])
-autoencoder.compile(loss="mse", optimizer=SGD(lr=0.01))
-plot_model(autoencoder, to_file='autoencoder.png', show_shapes=True)
+# get index of row which contains nan
+rows_with_nan = [index for index, row in train_data[:60].iterrows() if row.isnull().any()]
+print(rows_with_nan)
+
+# get df includes/excludes nan
+train_data_with_nan = train_data[:60].iloc[[i for i in range(train_data[:60].shape[0]) if i in rows_with_nan], :]
+train_data_wo_nan = train_data[:60].iloc[[i for i in range(train_data[:60].shape[0]) if i not in rows_with_nan], :]
+print(train_data_wo_nan)
+
+train_data_with_nan = train_data_with_nan.fillna(0)
+
+DAE = DenoisingAE()
+DAE.train(train_data_wo_nan, train_data_wo_nan)
+DAE.predict(train_data_with_nan)
+
+
 
