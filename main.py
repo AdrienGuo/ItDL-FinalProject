f1a554b8bf412708fb44508f49adf8bad1751612
Adrien Guo <gps.adrien@gmail.com>
2022-01-18 01:36:14 +0800

    norm

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from operator import concat
-from traceback import print_tb
 import pandas as pd
 import numpy as np
 
+from sklearn import preprocessing
 from keras.models import Sequential
+from keras.layers import GaussianNoise
 from keras.layers import Dense
 from keras.utils import plot_model
 
@@ -19,11 +19,51 @@ def split_dataframe(df):
     df_list = []
     for i in range(14):
         df_list.append(df[df['Asset_ID']==i])
-
     return df_list
 
 # This is a type(list)
-df_list = split_dataframe(train_data)
+df_ori = split_dataframe(train_data)
+df_list = df_ori
+
+# Normalization
+def normalization(df):
+    norm_Count = preprocessing.MinMaxScaler()
+    norm_Open = preprocessing.MinMaxScaler()
+    norm_High = preprocessing.MinMaxScaler()
+    norm_Low = preprocessing.MinMaxScaler()
+    norm_Close = preprocessing.MinMaxScaler()
+    norm_Volume = preprocessing.MinMaxScaler()
+    norm_VWAP = preprocessing.MinMaxScaler()
+
+    df[['Count']] = norm_Count.fit_transform(df[['Count']])
+    df[['Open']] = norm_Open.fit_transform(df[['Open']])
+    df[['High']] = norm_High.fit_transform(df[['High']])
+    df[['Low']] = norm_Low.fit_transform(df[['Low']])
+    df[['Close']] = norm_Close.fit_transform(df[['Close']])
+    df[['Volume']] = norm_Volume.fit_transform(df[['Volume']])
+    # df[['VWAP']] = norm_VWAP.fit_transform(df[['VWAP']])
+
+    dict = {
+        "Count": norm_Count,
+        "Open": norm_Open,
+        "High": norm_High,
+        "Low": norm_Low,
+        "Close": norm_Close,
+        "Volume": norm_Volume,
+        "VWAP": norm_VWAP,
+    }
+
+    return df, dict
+
+# Do Norm
+for i in range(len(df_list)):
+    dict_list = []
+    df, dict = normalization(df_list[i])
+    df_list[i] = df
+    dict_list.append(dict)
+
+print(df_list[11])
+
 
 # Split dataframe by nan
 def split_nan(df):
@@ -31,7 +71,6 @@ def split_nan(df):
     for idx in range(df.shape[0]):
         if df.iloc[idx].isnull().any():
             nan_list.append(idx)
-    print(nan_list)
     # rows_with_nan = [index for index, row in df.iterrows() if row.isnull().any()]
     # df_with_nan = df.iloc[[index for index, row in df.iterrows() if index in rows_with_nan], :]
     # df_wo_nan = df.iloc[[index for index, row in df.iterrows() if index not in rows_with_nan], :]
@@ -61,13 +100,16 @@ class DenoisingAE:
     def create_model(self, in_dim, out_dim):
         model = Sequential()
 
-        # encoder
-        model.add(Dense(units=5, input_dim=in_dim, activation="relu"))
+        # GaussianNoise
+        model.add(GaussianNoise(0.01, input_shape=(in_dim, )))
 
-        # decoder
+        # Encoder
+        model.add(Dense(units=5, activation="relu"))
+
+        # Decoder
         model.add(Dense(units=out_dim))
 
-        plot_model(model, to_file='autoencoder.png', show_shapes=True)
+        plot_model(model, to_file='Denoising_AutoEncoder.png', show_shapes=True)
         return model
 
     def train(self, data, label):
@@ -75,7 +117,7 @@ class DenoisingAE:
         out_dim = label.shape[1]
         self.model = self.create_model(in_dim, out_dim)
         self.model.compile(optimizer="adam", loss="mse")
-        data = data + self.gaussian_noise(data.shape)               # add gaussian noise to data
+        # data = data + self.gaussian_noise(data.shape)               # Add gaussian noise to data
         train_history = self.model.fit(x=data, y=label, epochs=10, batch_size=2)
     
     def predict(self, test):
@@ -86,9 +128,6 @@ df_with_nan = df_with_nan.fillna(0)
 DAE = DenoisingAE()
 DAE.train(df_wo_nan, df_wo_nan)
 predictions = DAE.predict(df_with_nan)
-print(predictions)
-
-# df = df_list[11].sort_values(by=['timestamp'])
 
 def add_predictions(df_with_nan, predictions):
     pred_list = [predictions[i][-1] for i in range(predictions.shape[0])]
@@ -101,8 +140,12 @@ def add_predictions(df_with_nan, predictions):
     return concat_df
 
 df_with_nan = add_predictions(df_with_nan, predictions)
-print(df_with_nan)
 
 def merge(df_with_nan, df_wo_nan):
-    
-    pass
+    concat_df = pd.concat([df_with_nan, df_wo_nan])
+    df = concat_df.sort_values(by=['timestamp'])
+    df = df.reset_index(drop=True)
+    return df
+
+df_all = merge(df_with_nan, df_wo_nan)
+print(df_all)
